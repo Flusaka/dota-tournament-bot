8ee37a81e11e6d07a26a854e32036e515780f8c5
Neil Armitage <flusaka@gmail.com>
2021-10-08 01:06:56 +0300

    Setting up heroku properly...

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);

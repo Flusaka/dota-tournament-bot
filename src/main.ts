3b0d68ae8b1dfe77a9b9d028147b768a427a9346
Neil Armitage <flusaka@gmail.com>
2021-10-10 19:27:17 +0300

    Actual fix for parsing PEM key

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ dotenv.config();
 admin.initializeApp({
     credential: admin.credential.cert({
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace('\\n', '\n'),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         projectId: process.env.FIREBASE_PROJECT_ID
     }),
     databaseURL: process.env.FIREBASE_DATABASE_URL
